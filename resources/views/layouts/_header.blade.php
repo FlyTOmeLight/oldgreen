74a879269070b568346f8d3d424f0d189ffc0438
zhoubohan <m18618474796@163.com>
2018-07-30 08:23:05 +0000

    注册和登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -14,9 +14,32 @@
 		<div class="collapse navbar-collapse" id="app-navbar-collapse">
 			<ul class="nav navbar-nav">
 			</ul>
+			{{-- 登录注册 --}}
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">登录</a></li>
-				<li><a href="#">注册</a></li>
+				@guest
+					<li><a href="{{ route('login') }}">登录</a></li>
+					<li><a href="{{ route('register') }}">注册</a></li>
+				@else 
+					<li class="dropdown-menu">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="falae">
+							<span class="user-avatar pull-left" style="margin-right: 9px; margin-top: -5px">
+								<img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+							</span>
+							{{ Auth::user()->name }} <span class="caret"></span>
+						</a>
+						<ul class="dropdown-menu" role="menu">
+							<li>
+								<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+									退出登录
+								</a>
+								<form action="{{ route('logout') }}" method="POST" style="display: none;" id="logout-form">
+									{{ csrf_field() }}
+								</form>
+							</li>
+						</ul>
+					</li>
+				@endguest
+				{{-- 登录注册结束 --}}
 			</ul>
 		</div>
 	</div>
